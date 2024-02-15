d99c2fbeba58c5a33f528898031a991987901dea
Aditi-Datta <aditidatta373@gmail.com>
2024-02-15 11:43:17 +0600

    redux using react

---
+++ Redux_With_React/counter-app/src/components/Counter.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+const Counter = () => {
+ 
+  const handleIncrement = () => {
+   
+  }
+
+  return (
+    <>
+    <div>
+    
+    <h1>Counter APP</h1>
+
+    <h3>Count: 0</h3>
+
+    <button onClick={handleIncrement}>Increment</button>
+
+    </div>
+    </>
+  )
+}
+export default Counter;
